9ddd1bda3b1892b08a65d1de9b36cde124eb3dab
German Hernandez <muygrafico@gmail.com>
2015-10-10 18:50:21 -0500

    create basic spec test

---
+++ lib/api_constraints.rb
@@ -0,0 +1,11 @@
+# api_constraints.rb
+class ApiConstraints
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || req.headers['Accept'].include?("application/vnd.marketplace.v#{@version}")
+  end
+end
